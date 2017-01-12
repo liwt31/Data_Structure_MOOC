7d254f87f6b5e33c85ac7b0befd85047f01267a2
liwt31 <liwt31@163.com>
2017-01-12 10:00:37 +0800

    Toy completed
    small bug in subscript wasted a lot of time.......
    Queue Hash object-oriented Toy......
    still can not figure out why crash.cpp will crash

--- Toy/main.cpp
+++ Toy/main.cpp
@@ -1,57 +1,248 @@
 #include <iostream>
+#include <cstdlib>
+#include <cstdio>
 
 using namespace std;
 
+template<typename T>
+struct Qnode{
+    T data;
+    Qnode* next;
+
+    Qnode<T>(T d):data(d),next(NULL){};
+};
+
+template<typename T>
+class Queue{
+    Qnode<T>* front_ptr;
+    Qnode<T>* end_ptr;
+public:
+    Queue<T>();
+    bool IsEmpty()const{return front_ptr==NULL;}
+    void Push(T &data);
+    T Pop();
+};
+
+template<typename T>
+Queue<T>::Queue(){
+    front_ptr=end_ptr=NULL;
+}
+
+template<typename T>
+void Queue<T>::Push(T &data){
+    Qnode<T> *new_node=new Qnode<T>(data);
+    if(!IsEmpty()){
+        end_ptr->next=new_node;
+    }else{
+        front_ptr=new_node;
+    }
+    end_ptr=new_node;
+}
+
+template<typename T>
+T Queue<T>::Pop(){
+    if(front_ptr==end_ptr){
+        end_ptr=NULL;
+    }
+    Qnode<T> *old_front=front_ptr;
+    front_ptr=front_ptr->next;
+    T bak=old_front->data;
+    delete old_front;
+    return bak;
+}
+
+
 class Toy{
     int toy[2][4];
+protected:
+    void TransformA(Toy &new_toy)const;
+    void TransformB(Toy &new_toy)const;
+    void TransformC(Toy &new_toy)const;
 public:
- //   friend class Toy;
+    int step;
+
+    template<typename T> friend class Hash;
+
+    Toy():step(-1){};
+    Toy(const Toy &t2){*this=t2;}
     bool operator==(const Toy &t2)const;
-    void SetToy(int r, int n){(r>4? toy[0][r%4]:toy[1][3-r%4])=n;}
-    void TransformA();
-    void TransformB();
-    void TransformC();
+    void print()const;
+    Toy& operator=(const Toy &t2);
+    void SetToy(int r, int n){(r<4? toy[0][r%4]:toy[1][3-r%4])=n;}
+    Toy Transform(int type)const;
+
 };
 
 bool Toy::operator==(const Toy &t2)const{
-    for(int i=0;i<8;i++){
-        if(toy[i]!=t2.toy[i]){
-            return false;
+    for(int i=0;i<2;i++){
+        for(int j=0;j<4;j++){
+            if(toy[i][j]!=t2.toy[i][j]){
+                return false;
+            }
         }
     }
     return true;
 }
 
-void Toy::TransformA(){
-    int temp[4];
+void Toy::print()const{
+    for(int r=0;r<8;r++){
+        cout<<(r<4? toy[0][r%4]:toy[1][3-r%4])<<" ";
+    }
+    cout<<endl;
+}
+
+Toy& Toy::operator=(const Toy &t2){
+    if(this!=&t2){
+        for(int i=0;i<2;i++){
+            for(int j=0;j<4;j++){
+                toy[i][j]=t2.toy[i][j];
+            }
+        }
+        step=t2.step;
+    }
+    return *this;
+}
+
+Toy Toy::Transform(int type)const{
+    Toy new_toy=*this;
+    switch(type){
+    case(0):
+    TransformA(new_toy);
+    break;
+    case(1):
+    TransformB(new_toy);
+    break;
+    case(2):
+    TransformC(new_toy);
+    break;
+    default:
+    break;
+    }
+    new_toy.step=step+1;
+    return new_toy;
+}
+
+void Toy::TransformA(Toy &new_toy)const{
     for(int i=0;i<4;i++){
-        temp[i]=toy[0][i];
-        toy[0][i]=toy[1][i];
-        toy[1][i]=temp[i];
+        int temp=new_toy.toy[0][i];
+        new_toy.toy[0][i]=new_toy.toy[1][i];
+        new_toy.toy[1][i]=temp;
     }
 }
 
-void Toy::TransformB(){
-    int temp0=toy[0][3];
-    int temp1=toy[1][3];
-    for(int i=3;i>0;i--){
-        toy[0][i+1]=toy[0][i];
-        toy[1][i+1]=toy[0][i];
+void Toy::TransformB(Toy &new_toy)const{
+    int temp0=new_toy.toy[0][0];
+    int temp1=new_toy.toy[1][0];
+    for(int i=0;i<3;i++){
+        new_toy.toy[0][i]=new_toy.toy[0][i+1];
+        new_toy.toy[1][i]=new_toy.toy[1][i+1];
     }
-    toy[0][0]=temp0;
-    toy[1][0]=temp0;
+    new_toy.toy[0][3]=temp0;
+    new_toy.toy[1][3]=temp1;
 }
 
-void Toy::TransformC(){
-    int temp=toy[0][1];
-    toy[0][1]=toy[1][1];
-    toy[1][1]=toy[1][2];
-    toy[1][2]=toy[0][2];
-    toy[0][2]=temp;
+void Toy::TransformC(Toy &new_toy)const{
+    int temp=new_toy.toy[0][1];
+    new_toy.toy[0][1]=new_toy.toy[0][2];
+    new_toy.toy[0][2]=new_toy.toy[1][2];
+    new_toy.toy[1][2]=new_toy.toy[1][1];
+    new_toy.toy[1][1]=temp;
+}
+
+
+template<typename T>
+class Hash{
+    T *bucket;
+    bool *occupation;
+
+protected:
+    int sz;
+    int HashFunc(const T &t)const;
+public:
+    Hash(int s);    //s should be primal, determined by the user
+    T& Find(const T &t)const;
+    bool Insert(const T &t);
+};
+
+template<typename T>
+Hash<T>::Hash(int s):sz(s){
+    bucket=new T[s];
+    occupation=new bool[s];
+    for(int i=0;i<s;i++){
+        occupation[i]=false;
+    }
+}
+
+template<typename T>
+T& Hash<T>::Find(const T &t)const{
+    int rank=HashFunc(t);
+    while(occupation[rank]){
+        if(bucket[rank]==t){
+            return bucket[rank];
+        }
+        rank++;
+    }
+    return bucket[rank];
+}
+
+template<typename T>
+bool Hash<T>::Insert(const T &t){
+    int rank=HashFunc(t);
+    while(occupation[rank]){
+        if(bucket[rank]==t){
+            return false;
+        }
+        rank++;
+    }
+    bucket[rank]=t;
+    return occupation[rank]=true;
+}
+
+template<>
+int Hash<Toy>::HashFunc(const Toy &t)const{
+    int result=0;
+    for(int i=0;i<2;i++){
+        for(int j=0;j<4;j++){
+            result*=9;
+            result+=t.toy[i][j];
+        }
+    }
+    srand(result);
+    return rand()%sz;
 }
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    Hash<Toy> database(130003);
+    Toy playing;
+    for(int i=0;i<8;i++){
+//        int temp;scanf("%d",&temp);playing.SetToy(i,temp);
+        playing.SetToy(i,i+1);
+    }
+    playing.step=0;
+    database.Insert(playing);
+    Queue<Toy> q_toy;
+    q_toy.Push(playing);
+    while(!q_toy.IsEmpty()){
+        Toy parent=q_toy.Pop();
+        for(int i=0;i<3;i++){
+            Toy child=parent.Transform(i);
+            if(database.Insert(child)){
+//                child.print();
+                q_toy.Push(child);
+            }
+        }
+    }
+    int N;scanf("%d",&N);
+    for(int i=0;i<N;i++){
+        Toy target;
+        for(int j=0;j<8;j++){
+            int temp;
+            scanf("%d",&temp);
+            target.SetToy(j,temp);
+        }
+//        database.Find(target).print();
+        printf("%d\n",database.Find(target).step);
+    }
     return 0;
 }
---
+++ test/main.cpp
@@ -0,0 +1,260 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+#include <fstream>
+
+using namespace std;
+
+ofstream fout("log.txt");
+
+template<typename T>
+struct Qnode{
+    T data;
+    Qnode* next;
+
+    Qnode<T>(T d):data(d),next(NULL){};
+};
+
+template<typename T>
+class Queue{
+    Qnode<T>* front_ptr;
+    Qnode<T>* end_ptr;
+public:
+    Queue<T>():front_ptr(NULL),end_ptr(NULL){};
+    bool IsEmpty()const{return front_ptr==NULL;}
+    void Push(T &data);
+    T Pop();
+};
+
+template<typename T>
+void Queue<T>::Push(T &data){
+    Qnode<T> *new_node=new Qnode<T>(data);
+    fout<<"new "<<new_node<<endl;
+    if(!IsEmpty()){
+        end_ptr->next=new_node;
+    }else{
+        front_ptr=new_node;
+    }
+    end_ptr=new_node;
+}
+
+template<typename T>
+T Queue<T>::Pop(){
+    fout<<"Pop() entered"<<endl;
+    if(front_ptr==end_ptr){
+        end_ptr=NULL;
+    }
+    fout<<"single element judge done"<<endl;
+    Qnode<T> *old_front=front_ptr;
+    front_ptr=front_ptr->next;
+    T bak=old_front->data;
+    fout<<"pointer setting done"<<endl;
+    Qnode<T> *ptr_bak=old_front;
+    fout<<"bak of deleting "<<ptr_bak<<endl;
+    fout<<"deleting "<<old_front<<" ";
+    fout<<"deleting "<<old_front<<" ";
+    delete ptr_bak;
+    fout<<"delete done"<<endl;
+    old_front=NULL;
+    return bak;
+}
+
+
+class Toy{
+    int toy[2][4];
+protected:
+    void TransformA(Toy &new_toy)const;
+    void TransformB(Toy &new_toy)const;
+    void TransformC(Toy &new_toy)const;
+public:
+    int step;
+
+    template<typename T> friend class Hash;
+
+    Toy():step(-1){};
+    Toy(const Toy &t2){*this=t2;}
+    bool operator==(const Toy &t2)const;
+    void print()const;
+    Toy& operator=(const Toy &t2);
+    void SetToy(int r, int n){(r<4? toy[0][r%4]:toy[1][3-r%4])=n;}
+    Toy Transform(int type)const;
+
+};
+
+bool Toy::operator==(const Toy &t2)const{
+    for(int i=0;i<2;i++){
+        for(int j=0;j<4;j++){
+            if(toy[i][j]!=t2.toy[i][j]){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+void Toy::print()const{
+    for(int r=0;r<8;r++){
+        cout<<(r<4? toy[0][r%4]:toy[1][3-r%4])<<" ";
+    }
+    cout<<endl;
+}
+
+Toy& Toy::operator=(const Toy &t2){
+    for(int i=0;i<2;i++){
+        for(int j=0;j<4;j++){
+            toy[i][j]=t2.toy[i][j];
+        }
+    }
+    step=t2.step;
+    return *this;
+}
+
+Toy Toy::Transform(int type)const{
+    Toy new_toy=*this;
+    switch(type){
+    case(0):
+    TransformA(new_toy);
+    break;
+    case(1):
+    TransformB(new_toy);
+    break;
+    case(2):
+    TransformC(new_toy);
+    break;
+    default:
+    break;
+    }
+    new_toy.step=step+1;
+    return new_toy;
+}
+
+void Toy::TransformA(Toy &new_toy)const{
+    for(int i=0;i<4;i++){
+        int temp=new_toy.toy[0][i];
+        new_toy.toy[0][i]=new_toy.toy[1][i];
+        new_toy.toy[1][i]=temp;
+    }
+}
+
+void Toy::TransformB(Toy &new_toy)const{
+    int temp0=new_toy.toy[0][3];
+    int temp1=new_toy.toy[1][3];
+    for(int i=2;i>=0;i--){
+        new_toy.toy[0][i+1]=new_toy.toy[0][i];
+        new_toy.toy[1][i+1]=new_toy.toy[1][i];
+    }
+    new_toy.toy[0][0]=temp0;
+    new_toy.toy[1][0]=temp1;
+}
+
+void Toy::TransformC(Toy &new_toy)const{
+    int temp=new_toy.toy[0][1];
+    new_toy.toy[0][1]=new_toy.toy[1][1];
+    new_toy.toy[1][1]=new_toy.toy[1][2];
+    new_toy.toy[1][2]=new_toy.toy[0][2];
+    new_toy.toy[0][2]=temp;
+}
+
+
+template<typename T>
+class Hash{
+    T *bucket;
+    bool *occupation;
+
+protected:
+    int sz;
+    int HashFunc(const T &t)const;
+public:
+    Hash(int s);    //s should be primal, determined by the user
+    T& Find(const T &t)const;
+    bool Insert(const T &t);
+};
+
+template<typename T>
+Hash<T>::Hash(int s):sz(s){
+    bucket=new T[s];
+    occupation=new bool[s];
+    for(int i=0;i<s;i++){
+        occupation[i]=false;
+    }
+}
+
+template<typename T>
+T& Hash<T>::Find(const T &t)const{
+    int rank=HashFunc(t);
+    while(occupation[rank]){
+        if(bucket[rank]==t){
+            return bucket[rank];
+        }
+        rank++;
+    }
+    return bucket[rank];
+}
+
+template<typename T>
+bool Hash<T>::Insert(const T &t){
+    int rank=HashFunc(t);
+    while(occupation[rank]){
+        if(bucket[rank]==t){
+            return false;
+        }
+        rank++;
+    }
+    bucket[rank]=t;
+    return occupation[rank]=true;
+}
+
+template<>
+int Hash<Toy>::HashFunc(const Toy &t)const{
+    int result=0;
+    for(int i=1;i<2;i++){
+        for(int j=0;j<4;j++){
+            result*=9;
+            result+=t.toy[i][j];
+        }
+    }
+    srand(result);
+    return rand()%sz;
+}
+
+int main()
+{
+    Hash<Toy> database(130003);
+    Toy playing;
+    for(int i=0;i<8;i++){
+//        int temp;scanf("%d",&temp);playing.SetToy(i,temp);
+        playing.SetToy(i,i+1);
+    }
+    playing.step=0;
+    database.Insert(playing);
+    Queue<Toy> q_toy;
+    q_toy.Push(playing);
+    while(!q_toy.IsEmpty()){
+        fout<<"preparing enter Pop()"<<endl;
+        Toy parent=q_toy.Pop();
+        fout<<"out of Pop()"<<endl;
+        for(int i=0;i<3;i++){
+            fout<<"enter trans cycle"<<endl;
+            Toy child=parent.Transform(i);
+            if(database.Insert(child)){
+//                child.print();
+                q_toy.Push(child);
+            }
+        }
+        fout<<"out of trans cycle"<<endl;
+    }
+    int N;scanf("%d",&N);
+    for(int i=0;i<N;i++){
+        Toy target;
+        for(int j=0;j<8;j++){
+            int temp;
+            scanf("%d",&temp);
+            target.SetToy(j,temp);
+            fout<<"SetToy done"<<endl;
+        }
+        fout<<"out of set cycle"<<endl;
+//        database.Find(target).print();
+        printf("%d\n",database.Find(target).step);
+    }
+    return 0;
+}
